954ac451e96456da68b2418e18ea86e60d17c06b
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-05-01 18:24:50 +0300

    Move bootstrap to public part

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const keyGenerator = require('./Utils/auth-key-generator');
 
 let app = express();
 app.use(express.static('public'));
-app.use('/libs', express.static('node_modules'));
 app.use(bodyParser.json());
 
 app.get("/api/recent", dataControllers.getRecent);
